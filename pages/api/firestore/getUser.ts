d22a4796077b86e95cfd2f04bfea691ede77b794
sugayutokyo <mygit_yutasugao20171126@yahoo.co.jp>
2022-02-23 14:15:09 +0000

    add createUser

---
+++ pages/api/firestore/getUser.ts
@@ -0,0 +1,22 @@
+const { cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const serviceAccount = require('../../../firebase-test-serviceAccount.json');
+const admin = require('firebase-admin');
+
+export default async (req: any, res: { statusCode: number }) => {
+  console.log('test');
+  
+  if (admin.apps.length === 0) {
+    admin.initializeApp({
+      credential: cert(serviceAccount),
+    });
+  }
+
+  const db = getFirestore();
+  const docRef = db.collection('users').doc('test2');
+  await docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815,
+  });
+};
